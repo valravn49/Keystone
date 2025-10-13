c8ce79abe8d076e39699b66256cd768e7720f3d2
valravn49 <nick.martin7011@gmail.com>
2025-10-13 13:08:27 +1100

    Create autonomy_state.py

---
+++ autonomy_state.py
@@ -0,0 +1,9 @@
+# Holds shared runtime state for all bots and systems
+state = {
+    "rotation_index": 0,
+    "theme_index": 0,
+    "last_theme_update": None,
+    "history": {},
+    "spontaneous_end_tasks": {},
+    "last_spontaneous_task": None,
+}
