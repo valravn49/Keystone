ad623265cc31745fd85901180fef24eedc819362
valravn49 <nick.martin7011@gmail.com>
2025-10-13 14:29:59 +1100

    Update autonomy_will.py

--- autonomy_will.py
+++ autonomy_will.py
@@ -1,12 +1,19 @@
+import os
+import discord
 from discord.ext import commands
 
-intents = commands.Intents.default()
+from logger import log_event
+
+# ✅ Correct: Intents are from `discord`, not `commands`
+intents = discord.Intents.default()
 intents.message_content = True
 intents.guilds = True
 
-class WillBot(commands.Bot):
-    def __init__(self, info):
-        super().__init__(command_prefix="!", intents=intents)
-        self.sister_info = info
 
-will_bot = WillBot({"name": "Will", "env_var": "WILL_TOKEN"})
+class WillBot(commands.Bot):
+    def __init__(self, will_info):
+        super().__init__(command_prefix="!", intents=intents)
+        self.sister_info = will_info
+
+    async def setup_hook(self):
+        log_event("[BOOT] Will bot setup hook initialized.")
